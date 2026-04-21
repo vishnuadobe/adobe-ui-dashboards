f0e4d9ca8f0905e309a138a2022785f4792387b0
vishnuadobe <vdivyeshan@adobe.com>
2026-04-21 14:00:52 +0530

    Add files via upload

---
+++ vendor/htm-preact.js
@@ -0,0 +1,4 @@
+import { h as r, Component as o, render as t } from './preact.js';
+import e from './htm.js';
+
+export { h, render, Component } from './preact.js'; const m = e.bind(r); export { m as html };
